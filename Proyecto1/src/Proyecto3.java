eb889aa4c22d4652c898349714f4bb5ef3ee1316
AllanUrbina <142625851+AllanUrbina@users.noreply.github.com>
2023-10-24 19:20:03 -0600

    Update Proyecto3.java

--- Proyecto1/src/Proyecto3.java
+++ Proyecto1/src/Proyecto3.java
@@ -1,34 +1,44 @@
 import java.util.Scanner;
- public class Proyecto3 {
+
+public class Proyecto3 {
+    static int totalRosquillas, rosquillasobrante, contenedores, cajas, cajasobrante, intentos;
+
     public static void main(String[] args) {
-        
-        
-     Scanner lector = new   Scanner(System.in);
-       
-     int totalRosquillas,rosquillasobrante,contenedores,cajas,cajasobrante;
-    
-    
-     System.out.print("Ingrese cantidad de rosquillas: ");
-        
-      totalRosquillas = lector.nextInt();
-     
-     
-     cajas = totalRosquillas / 24;
-        
-     rosquillasobrante = totalRosquillas % 24;
-        
-      contenedores = cajas / 75;
-        
-      cajasobrante= cajas % 75;
-        
-     System.out.println("Número de cajas que necesitan : " + cajas);
-        
-     System.out.println("Número de contenedores que necesita: " + contenedores);
-        
-     System.out.println("Rosquillas sobrantes: " + rosquillasobrante);
-        
-     System.out.println("Cajas de rosquillas sobrantes: " + cajasobrante);
-     
-        lector.close();
+
+        Scanner lector = new Scanner(System.in);
+
+        for (intentos = 0; intentos < 3; intentos++) {
+            System.out.print("Ingrese cantidad de rosquillas: ");
+
+            totalRosquillas = lector.nextInt();
+            if (totalRosquillas < 0) {
+                System.out.println("No se aceptan valores menores a 0.1");
+            } else {
+                intentos = 3;
+            }
+        }
+
+        if (intentos == 3) {
+            System.out.println("Demasiados intentos fallidos");
+        } else {
+
+            cajas = totalRosquillas / 24;
+
+            rosquillasobrante = totalRosquillas % 24;
+
+            contenedores = cajas / 75;
+
+            cajasobrante = cajas % 75;
+
+            System.out.println("Número de cajas que necesitan : " + cajas);
+
+            System.out.println("Número de contenedores que necesita: " + contenedores);
+
+            System.out.println("Rosquillas sobrantes: " + rosquillasobrante);
+
+            System.out.println("Cajas de rosquillas sobrantes: " + cajasobrante);
+
+            lector.close();
+        }
     }
 }
